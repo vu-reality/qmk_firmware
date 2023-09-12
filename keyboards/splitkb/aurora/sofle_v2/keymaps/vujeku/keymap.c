d0208175d6073b9ba9854218b72dd0c78890ed3e
Trash-VuCan <100589446+Trash-VuCan@users.noreply.github.com>
2023-09-12 16:29:49 +0200

    Update keymap.c

--- keyboards/splitkb/aurora/sofle_v2/keymaps/vujeku/keymap.c
+++ keyboards/splitkb/aurora/sofle_v2/keymaps/vujeku/keymap.c
@@ -12,9 +12,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT(
 	KC_ESC, 		KC_1, 		KC_2, 		KC_3, 		KC_4, 		KC_5, 								KC_6, 		KC_7, 		KC_8, 		KC_9, 		KC_0, 		KC_GRV,
 	KC_TAB, 		KC_Q, 		KC_W, 		KC_E, 		KC_R, 		KC_T, 								KC_Y, 		KC_U, 		KC_I, 		KC_O, 		KC_P, 		KC_SLSH,
-	KC_LSFT, 		KC_A, 		KC_S, 		KC_D, 		KC_F, 		KC_G, 								KC_H, 		KC_J, 		KC_K, 		KC_L, 		KC_SCLN,	KC_ESC,
+	KC_LSFT, 		KC_A, 		KC_S, 		KC_D, 		KC_F, 		KC_G, 								KC_H, 		KC_J, 		KC_K, 		KC_L, 		KC_SCLN,	KC_BSPC,
 	KC_LCTL, 		KC_Z, 		KC_X, 		KC_C, 		KC_V, 		KC_B, 	  KC_MUTE, 	KC_PSCR,		KC_N, 		KC_M, 		KC_COMM,	KC_DOT, 	KC_LALT,	KC_RSFT,
-										KC_LALT, KC_LGUI, MO(3), MO(1),   KC_SPC,   KC_ENT, 		MO(2), KC_BSPC, KC_ESC, KC_DEL),
+										KC_LALT, KC_LGUI, MO(3), MO(1),   KC_SPC,   KC_ENT, 		MO(2), KC_ESC, KC_ESC, KC_DEL),
 	[1] = LAYOUT(
 	KC_TRNS, 		KC_TRNS, 	KC_TRNS, 	KC_TRNS, 	KC_TRNS, 	KC_TRNS, 							KC_TRNS, 	KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO,
 	KC_TRNS, 		KC_EXLM, 	KC_AT, 		KC_HASH, 	KC_DLR 	, 	KC_PERC, 							KC_CIRC, 	KC_AMPR, 	KC_ASTR, 	KC_QUES,	KC_PIPE, 	KC_TRNS,
